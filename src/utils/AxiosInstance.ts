2938e880ff6612abca0690790b662e139cbe1c21
Amitai Zand <amitai17@gmail.com>
2023-09-25 22:33:14 +0300

    Still fixing Axios baseURL

--- src/utils/AxiosInstance.ts
+++ src/utils/AxiosInstance.ts
@@ -4,12 +4,8 @@ const instance = axios.create({
     validateStatus: status => {
         return status < 400;
     },
-    baseURL:
-        // Switch backend URL depending on environment
-        process.env.NODE_ENV === "development"
-            ? "http://localhost:5194/api"
-            : "https://moah-api.azurewebsites.net/api",
-    withCredentials: process.env.NODE_ENV === "development" ? false : true,
+    baseURL: "https://moah-api.azurewebsites.net/api",
+    withCredentials: true,
 });
 
 export default instance;
